d95e4227968966aa0584c39286128f4b729867b4
vairai <vairai@iu.edu>
2026-03-13 16:24:54 -0400

    Updated Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -85,7 +85,7 @@ const EXPERIENCE = [
   },
   {
     id: "qk-sre",
-    title: "Software Engineer -Site Reliability Engineer",
+    title: "Software Engineer -Site Reliability",
     company: "QualityKiosk Technologies, India",
     date: "2022 – 2023",
     location: "India",
